3de544556ddadb9779fef0d52369a51147523dbf
kwok-jh <kwok-jh@qq.com>
2018-03-22 09:36:55 +0800

    modified project config

--- include/iocp.h
+++ include/iocp.h
@@ -43,20 +43,17 @@ public:
         m_hIOCP = NULL;
     }
 
-    //创建完成端口，设置线程并发数量
     bool Create(int nMaxConcurrency = 0) 
     {
         m_hIOCP = ::CreateIoCompletionPort( INVALID_HANDLE_VALUE, NULL, 0, nMaxConcurrency);
         return m_hIOCP != NULL;
     }
 
-    //将设备、完成键（自定义的指针）关联完成端口
     bool AssociateDevice(HANDLE hDevice, ULONG_PTR CompKey) 
     {
         return ::CreateIoCompletionPort(hDevice, m_hIOCP, CompKey, 0) == m_hIOCP;
     }
     
-    //将socket关联完成端口
     bool AssociateSocket(SOCKET hSocket, ULONG_PTR CompKey) 
     {
         return AssociateDevice((HANDLE) hSocket, CompKey);
@@ -75,15 +72,13 @@ public:
     {
         //_in    pCompKey        与设备关联时设置的指针
         //_in    dwMilliseconds  操作等待时间 INFINITE：无限等待
-        //_out    pdwNumBytes    操作完成的字节数
-        //_out    ppo            重叠I/O结构指针,投放I/O操作时设置的指针 
+        //_out   pdwNumBytes     操作完成的字节数
+        //_out   ppo             重叠I/O结构指针,投放I/O操作时设置的指针 
         //                       若传入的是一个包含OVERLAPPED的对象 
         //                       则可以使用CONTAINING_RECORD获得该结构的指针。
         return !!::GetQueuedCompletionStatus(m_hIOCP, pdwNumBytes, pCompKey, ppo, dwMilliseconds);
     }
+
 private:
     HANDLE m_hIOCP;
 };
-
-
-///////////////////////////////// End of File /////////////////////////////////
